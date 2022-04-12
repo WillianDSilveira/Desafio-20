334d4a1c103cdfe48dae78c7e63bff0e60c009ae
Willian Dalferth da Silveira <williansilveira100@hotmail.com>
2022-04-11 21:05:52 -0300

    add alert

--- menu.js
+++ menu.js
@@ -2,11 +2,17 @@ let show = true;
 
 const menuSection = document.querySelector(".menu-section")
 const menuToggle = menuSection.querySelector(".menu-toggle")
+const liActive = menuSection.querySelector(".nav")
 
 menuToggle.addEventListener("click", () => {
 
     document.body.style.overflow = show ? "hidden" : "initial";
 
+    menuSection.classList.toggle("on", show) 
+    show = !show;
+})
+
+liActive.addEventListener("click", () => {        
     menuSection.classList.toggle("on", show)
     show = !show;
 })
---
+++ script.js
@@ -0,0 +1,5 @@
+const url = window.location.href
+
+if(url === "http://127.0.0.1:5500/index.html#Obrigado") {
+    alert("Email enviado com sucesso!");
+}
